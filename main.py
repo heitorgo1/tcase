2eae05533758a9bece4207755ac0c606b90e3c3c
Heitor <heitor1994.hr@gmail.com>
2018-02-06 02:20:58 -0300

    changes

--- main.py
+++ main.py
@@ -7,18 +7,23 @@ Description:
 Usage:
   tcase [--contest]
         [--online-judge=JUDGE]
+        [--output-dir=OUT_DIR]
         <problem_or_contest_ids>...
 
 Options:
   -c, --contest                     Contest mode.
-  -o, --online-judge=JUDGE          Online judge name. [default: codeforces]
+  -o, --online-judge=JUDGE          Online judge name (uva, uri or cf). [default: codeforces]
+  -od, --output-dir=OUT_DIR         Output directory
 """
 import os, errno
-from docopt import docopt
 import config
 import requests
-from bs4 import BeautifulSoup
 import subprocess
+import tempfile
+
+from sys import exit
+from docopt import docopt
+from bs4 import BeautifulSoup
 
 def create_folder(directory):
     try:
@@ -27,44 +32,54 @@ def create_folder(directory):
         if e.errno != errno.EEXIST:
             raise
 
-def readPdfFile(filepath):
-    proc = subprocess.Popen(['pdftotext', '-raw' , filepath, '-'], stdout=subprocess.PIPE)
-    text = proc.stdout.read().decode('utf-8')
-    return text
 
+def readPdfFile(filepath):
+    proc = subprocess.Popen(['pdftotext', '-raw', filepath, '-'], stdout=subprocess.PIPE)
+    text = proc.stdout.read()
+    return text.decode('utf-8','ignore')
 
 
 class Problem:
 
-    def __init__(self, id, name, timelimit, testcases=[]):
+    def __init__(self, id, name, timelimit, onlinejudge, testcases=[]):
         self.id = id
         self.name = name
         self.timelimit = timelimit
         self.testcases = testcases
+        self.onlinejudge = onlinejudge
 
-    def addTestCase (self, testcase):
+    def addTestCase(self, testcase):
         self.testcases.append(testcase)
 
-    def printProblem(self):
-        input_folder = '{}/{}/input'.format(os.getcwd(), self.id)
-        output_folder = '{}/{}/output'.format(os.getcwd(), self.id)
+    def printProblem(self, out_dir):
+        out_dir = os.path.expanduser(out_dir)
+        input_folder = '{}/{}/input'.format(out_dir, self.id)
+        output_folder = '{}/{}/output'.format(out_dir, self.id)
         create_folder(input_folder)
         create_folder(output_folder)
 
-        with open(os.path.join(os.getcwd(), self.id,  'info.txt'), 'w') as f:
+        with open(os.path.join(out_dir, self.id, 'info.txt'), 'w') as f:
             f.write('ID={}\n'.format(self.id))
             f.write('NAME={}\n'.format(self.name))
             f.write('TIMELIMIT={}\n'.format(self.timelimit))
+            f.write('OJ={}\n'.format(self.onlinejudge))
 
         for index, case in enumerate(self.testcases):
-            with open(os.path.join(input_folder, str(index)+'.in'), 'w') as f:
+            with open(os.path.join(input_folder, str(index) + '.in'), 'w') as f:
                 f.write(case[0]);
 
-            with open(os.path.join(output_folder, str(index)+'.out'), 'w') as f:
+            with open(os.path.join(output_folder, str(index) + '.out'), 'w') as f:
                 f.write(case[1]);
 
-        def __str__(self):
-            return (self.id, self.name, self.timelimit, self.testcases).__str__()
+        script_dir, _ = os.path.split(os.path.abspath(__file__))
+        with open(os.path.join(script_dir, 'templates', 'cplusplus.cpp'), 'r') as f:
+            code_path = os.path.join(out_dir, self.id, 'sol.cpp')
+            if not os.path.isfile(code_path):
+                with open(code_path, 'w') as cpp:
+                    cpp.write(f.read())
+
+    def __str__(self):
+        return (self.id, self.name, self.timelimit, self.onlinejudge, self.testcases).__str__()
 
 
 class CfContestParser:
@@ -72,29 +87,27 @@ class CfContestParser:
 
 
 class UvaProblemParser:
-
     host = config.CONFIG['uva']['host']
 
     def __init__(self, problem_id):
         prefix = problem_id[:-2]
-        path = config.CONFIG['uva']['formats']['problem'].format(self.host,prefix,problem_id)
+        path = config.CONFIG['uva']['formats']['problem'].format(self.host, prefix, problem_id)
         req = requests.get(path)
 
         if req.status_code != 200:
             raise Exception('Invalid page: {}'.format(path))
-        
-        with open('/tmp/{}.pdf'.format(problem_id), 'wb') as fd:
+
+        with open(os.path.join(tempfile.gettempdir(), '{}.pdf'.format(problem_id)), 'wb') as fd:
             fd.write(req.content)
-        
+
         self.problem_id = problem_id
 
-    
     def to_problem(self):
         stats = requests.get(config.CONFIG['uva']['formats']['stats'].format(self.problem_id)).json()
-        raw_text = readPdfFile('/tmp/{}.pdf'.format(self.problem_id))
+        raw_text = readPdfFile(os.path.join(tempfile.gettempdir(), '{}.pdf'.format(self.problem_id)))
         id = self.problem_id
         name = stats['title']
-        timelimit = int(stats['rtl']/1000)
+        timelimit = int(stats['rtl'] / 1000)
 
         input_idx = raw_text.index('Sample Input')
         raw_text = raw_text[input_idx:]
@@ -102,6 +115,7 @@ class UvaProblemParser:
         isOutput = False
         input_lines = []
         output_lines = []
+
         for line in raw_text.splitlines():
             if line == 'Sample Input':
                 isInput = True
@@ -111,30 +125,27 @@ class UvaProblemParser:
                 isOutput = True
                 isInput = False
                 continue
-            if '{} – {}'.format(id, name) in line:
+            if '{}'.format(name) in line and '{}'.format(id) in line:
                 continue
             if not line:
                 continue
-            
+
             if isInput:
                 input_lines.append(line)
             elif isOutput:
                 output_lines.append(line)
-        
-        testcases = [('\n'.join(input_lines)+'\n', '\n'.join(output_lines)+'\n')]
 
-        return Problem(id,name,timelimit,testcases)
+        testcases = [('\n'.join(input_lines) + '\n', '\n'.join(output_lines) + '\n')]
 
-
+        return Problem(id, name, timelimit, 'UVA', testcases)
 
 
 class CfProblemParser:
-
     host = config.CONFIG['cf']['host']
 
     def __init__(self, problem_id):
         problem_id = problem_id.upper()
-        path = config.CONFIG['cf']['formats']['problem'].format(self.host,problem_id[:-1],problem_id[-1])
+        path = config.CONFIG['cf']['formats']['problem'].format(self.host, problem_id[:-1], problem_id[-1])
         req = requests.get(path)
 
         if req.status_code != 200:
@@ -167,11 +178,10 @@ class CfProblemParser:
 
         testcases = list(zip(sample_input, sample_output))
 
-        return Problem(id,name,timelimit,testcases)
+        return Problem(id, name, timelimit, 'Codeforces', testcases)
 
 
 class UriProblemParser:
-
     host = config.CONFIG['uri']['host']
 
     def __init__(self, problem_id):
@@ -201,24 +211,33 @@ class UriProblemParser:
         ins = [''.join([self.str_tag(y) for y in x]) for x in ins]
         outs = [x.next_sibling.next_sibling.p.contents for x in testcases]
         outs = [''.join([self.str_tag(y) for y in x]) for x in outs]
-        testcases = list(zip(ins,outs));
+        testcases = list(zip(ins, outs));
 
-        return Problem(id,name,timelimit,testcases)
+        return Problem(id, name, timelimit, 'URI', testcases)
+
 
 if __name__ == '__main__':
     options = docopt(__doc__, version='1.0.0', options_first=True)
 
-    if options['--online-judge'] in ['cf', 'codeforces']:
-        for problem_id in options['<problem_or_contest_ids>']:
-            problem = CfProblemParser(problem_id).to_problem()
-            problem.printProblem()
-    elif options['--online-judge'] in ['uva']:
-        for problem_id in options['<problem_or_contest_ids>']:
-            problem = UvaProblemParser(problem_id).to_problem()
-            problem.printProblem()
-    elif options['--online-judge'] in ['uri']:
-        for problem_id in options['<problem_or_contest_ids>']:
-            problem = UriProblemParser(problem_id).to_problem()
-            problem.printProblem()
-    else:
-        print('{} Not implemented.'.format(options['--online-judge']))
+    if not options['--output-dir']:
+        options['--output-dir'] = os.getcwd()
+
+    try:
+        if options['--online-judge'] in ['cf', 'codeforces']:
+            for problem_id in options['<problem_or_contest_ids>']:
+                problem = CfProblemParser(problem_id).to_problem()
+                problem.printProblem(options['--output-dir'])
+        elif options['--online-judge'] in ['uva']:
+            for problem_id in options['<problem_or_contest_ids>']:
+                problem = UvaProblemParser(problem_id).to_problem()
+                problem.printProblem(options['--output-dir'])
+        elif options['--online-judge'] in ['uri']:
+            for problem_id in options['<problem_or_contest_ids>']:
+                problem = UriProblemParser(problem_id).to_problem()
+                problem.printProblem(options['--output-dir'])
+        else:
+            print('{} Not implemented.'.format(options['--online-judge']))
+    except Exception:
+        exit(1)
+
+    exit(0)
---
+++ templates/cplusplus.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int,int> ii;
+typedef vector<ii> vii;
+typedef vector<vii> vvii;
+typedef long long ll;
+
+const int inf = 1e9;
+
+int main () {
+
+    return 0;
+}
